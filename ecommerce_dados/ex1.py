b0cedfe7090122755201ed5130aa7de4783cd57d
Emanuelle Natal <emanuellenmoura@gmail.com>
2026-02-16 15:16:32 -0300

    novos exercicios, arquivos especificados como .py agr

---
+++ ecommerce_dados/ex1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('/data/ecommerce_tratados.csv')
+
+# Verifica a quantidade de dados únicos em cada coluna
+unicos = df.nunique()
+print('Analise de dados únicos:\n', unicos)
+
+# Calcula estatísticas descritivas dos campos numéricos
+estatisticas = df.describe()
+print('Estatísticas dos dados:\n', estatisticas)
+
+# Cria o campo "Preço" com o cálculo em relação aos campos "Reais" e "Centavos"
+df['Preco'] = df['Reais'] + (df['Centavos'] / 100)
+
+df = df.drop(columns=['Reais', 'Centavos', 'Condicao', 'Condicao_Atual'])
