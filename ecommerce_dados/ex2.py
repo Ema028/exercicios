e77ebae60a8b1ee9a4b0f632b300f1bdd6cf6912
Emanuelle Natal <156372524+Ema028@users.noreply.github.com>
2026-03-24 12:05:49 -0300

    comentários adicionados

--- ecommerce_dados/ex2.py
+++ ecommerce_dados/ex2.py
@@ -16,4 +16,5 @@ df['Temporada_Cod'] = LabelEncoder().fit_transform(df['Temporada'])
 # LabelEncoder → categorias → inteiros (cria ordem implícita)
 # OneHotEncoder → categorias → colunas binárias (sem ordem)
 # get_dummies → OneHotEncoder direto no pandas
-
+# MinMaxScaler -> espreme todos os seus dados para caberem dentro de uma caixa exata, geralmente entre 0 e 1.
+# StandardScaler -> ajusta a sua coluna para que a média de todos os valores passe a ser 0 e o desvio padrão seja 1.
